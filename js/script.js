4d16b2c28e3b502dc3e7c3684abad37ea45847cc
Marlo Wendell <mee213@gmail.com>
2019-01-21 12:51:29 -0600

    finished register for activities section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const main = () => {
     
+    // RESET ON PAGE LOAD
+
     $('#name').focus();
     $('#other-title').addClass('is-hidden');
     $('#design').prop('selectedIndex', 0);
@@ -7,6 +9,10 @@ const main = () => {
         $(this).removeClass('is-hidden');
     });
     $('#color').prop('selectedIndex', 0);
+    $('.activities input[type="checkbox"]').prop('checked', false);
+    $('.activities').append('<p class="total-due"></p>');
+
+    // "JOB ROLE" SECTION
 
     $('#title').change(function() {
         if ($(this).val() === 'other') {
@@ -15,6 +21,8 @@ const main = () => {
             $('#other-title').addClass('is-hidden');
         }
     });
+
+    // "T-SHIRT INFO" SECTION
 
     $('#design').change(function() {
 
@@ -50,6 +58,89 @@ const main = () => {
             }
         });
         
+    });
+
+    // "REGISTER FOR ACTIVITIES" SECTION
+
+    const $all = $('input[name="all"]');
+    const $jsFrameworks = $('input[name="js-frameworks"]');
+    const $jsLibs = $('input[name="js-libs"]');
+    const $express = $('input[name="express"]');
+    const $node = $('input[name="node"]');
+    const $buildTools = $('input[name="build-tools"]');
+    const $npm = $('input[name="npm"]');
+    const $totalDue = $('.total-due');
+    let totalDue = 0;
+
+    $all.change(function() {
+        if (this.checked) {
+            totalDue += 200;
+        } else {
+            totalDue -= 200;
+        }
+        $totalDue.text(`Total: $${totalDue}`);
+    })
+
+    $jsFrameworks.change(function() {
+        if (this.checked) {
+            $express.prop('disabled', true);
+            $express.parent().css('color', 'gray');
+            totalDue += 100;
+        } else {
+            $express.prop('disabled', false);
+            $express.parent().css('color', 'black');
+            totalDue -= 100;
+        } 
+        $totalDue.text(`Total: $${totalDue}`);
+    });
+
+    $express.change(function() {
+        if (this.checked) {
+            $jsFrameworks.prop('disabled', true);
+            $jsFrameworks.parent().css('color', 'gray');
+            totalDue += 100;
+        } else {
+            $jsFrameworks.prop('disabled', false);
+            $jsFrameworks.parent().css('color', 'black');
+            totalDue -= 100;
+        } 
+        $totalDue.text(`Total: $${totalDue}`);
+    });
+
+    $jsLibs.change(function() {
+        if (this.checked) {
+            $node.prop('disabled', true);
+            $node.parent().css('color', 'gray');
+            totalDue += 100;
+        } else {
+            $node.prop('disabled', false);
+            $node.parent().css('color', 'black');
+            totalDue -= 100;
+        } 
+        $totalDue.text(`Total: $${totalDue}`);
+    });
+
+    $node.change(function() {
+        if (this.checked) {
+            $jsLibs.prop('disabled', true);
+            $jsLibs.parent().css('color', 'gray');
+            totalDue += 100;
+        } else {
+            $jsLibs.prop('disabled', false);
+            $jsLibs.parent().css('color', 'black');
+            totalDue -= 100;
+        } 
+        $totalDue.text(`Total: $${totalDue}`);
+    });
+
+    $buildTools.change(function() {
+        this.checked ? totalDue += 100 : totalDue -= 100;
+        $totalDue.text(`Total: $${totalDue}`);
+    });
+
+    $npm.change(function() {
+        this.checked ? totalDue += 100 : totalDue -= 100;
+        $totalDue.text(`Total: $${totalDue}`);
     });
 
 }
